dce739f285943bee9c7d233d60d1477b1f4db3a7
Santo Rasu <santorasu09.com>
2025-02-17 22:55:28 +0600

    This is test of the app to check its work properly

--- lib/widget/module_12_class_1.dart
+++ lib/widget/module_12_class_1.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../model/expense.dart';
-
 class ExpenseTracker extends StatefulWidget {
   const ExpenseTracker({super.key});
 
@@ -10,117 +8,16 @@ class ExpenseTracker extends StatefulWidget {
 }
 
 class _ExpenseTrackerState extends State<ExpenseTracker> {
-  final List<Expense> _expense = [];
-  final List<String> _categorys = [
-    'Food',
-    'Travel',
-    'Entertainment',
-    'Bills',
-    'Transportation'
-  ];
-  final double _total = 0.0;
-
-
-  void _showForm(BuildContext context) {
-    TextEditingController titleController = TextEditingController();
-    TextEditingController amountController = TextEditingController();
-    String selectedCategory = _categorys.first;
-    showModalBottomSheet(
-        context: context,
-        builder: (_) {
-          return Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const TextField(
-                decoration: InputDecoration(labelText: 'Title'),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              const TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(labelText: 'Amount'),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-
-              DropdownButtonFormField<String>(
-                items: _categorys.map((category)=>DropdownMenuItem(value: category, child: Text(category))).toList(),
-                onChanged: (value) => selectedCategory =value!,
-                decoration: const InputDecoration(labelText: 'Category'),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      foregroundColor: Colors.white,
-                    ),
-                    onPressed: (){}, child: Text("Add Expense")),
-              ),
-
-              const SizedBox(
-                height: 20,
-              ),
-            ],
-          );
-        });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Expense Tracker'),
-        actions: [
-          IconButton(
-              onPressed: () => _showForm(context), icon: const Icon(Icons.add))
-        ],
+        title: const Text('Expense Details'),
+        centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Center(
-            child: Card(
-              margin: const EdgeInsets.all(20),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12)),
-              child: Padding(
-                padding: const EdgeInsets.all(36.0),
-                child: Text(
-                  "Total: \$$_total",
-                  style: const TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-                itemCount: _expense.length,
-                itemBuilder: (ctx, index) {
-                  return Card(
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Colors.blueAccent,
-                        child: Text(_expense[index].category),
-                      ),
-                      title: Text(_expense[index].title),
-                      subtitle: Text(_expense[index].date.toString()),
-                    ),
-                  );
-                }),
-          )
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _showForm(context),
-        child: const Icon(Icons.add),
+      body: const Center(
+        child: Text('This is the Expense Details Page'),
       ),
     );
   }
